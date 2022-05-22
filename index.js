a54509c9e71cf9166412090f432784518afc6b49
MD Limon Farazi <abidislamabir@gmail.com>
2022-05-22 21:27:06 +0600

    second update

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0cm0o.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0cm0o.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -23,22 +22,59 @@ async function run() {
     const collectionDatabase = client.db("person").collection("user");
 
     // get and read all api
-    app.get('/notes', async(req,res)=>{
+    app.get("/notes", async (req, res) => {
       const query = req.body;
-      console.log(query)
+      // console.log(query)
       const cursor = collectionDatabase.find({});
       const result = await cursor.toArray();
       res.send(result);
     });
-    // Create api 
-    app.post('/note', async(req,res)=>{
+    // Create api
+    app.post("/note", async (req, res) => {
+      const query = req.body;
+      // console.log(data);
+      const result = await collectionDatabase.insertOne(query);
+      res.send(result);
+    });
+    // UPDATE api
+    app.put("/note/:id", async (req, res) => {
+      const id = req.params.id;
       const data = req.body;
-      console.log(data);
-      const result = await collectionDatabase.insertOne(data);
+      console.log("form updating api", data);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          // দুইটা কোড আছে একটা ব্যবহার করলেই হবে
+
+          ...data,
+
+          // userName: data.userName,
+          // userAddress: data.userAddress,
+        },
+      };
+      const result = await collectionDatabase.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      // console.log('form put method', id);
       res.send(result);
     });
 
-
+    // DELETE api
+    app.delete("/note/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await collectionDatabase.deleteOne(filter);
+    
+      if (result.deletedCount === 1) {
+        console.log("Successfully deleted one document.", id);
+      } else {
+        console.log("No documents matched the query. Deleted 0 documents.", id);
+      }
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
